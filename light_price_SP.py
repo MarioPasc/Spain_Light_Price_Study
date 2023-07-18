38e6b031a81ae0690d27054a5915789d59b40db2
Mawio <120520768+Mawio02@users.noreply.github.com>
2023-07-18 12:18:26 +0200

    Add files via upload
    This file includes the calls for the esios company, they provide the data via API

---
+++ light_price_SP.py
@@ -0,0 +1,44 @@
+import json
+import requests
+from datetime import date
+import os
+
+#region Datos propios de la solicitud
+TOKEN = "015e8781f5ece93118fff662ab82dcfb41e3c001d8e63538dadb4c99256a3291"
+url = 'https://api.esios.ree.es/indicators/1001'
+headers = {'Accept': 'application/json; application/vnd.esios-api-v2+json',
+           'Content-Type': 'application/json',
+           'Host': 'api.esios.ree.es',
+           'Authorization': 'Token token=\"TOKEN\"'}
+#endregion
+
+
+def http_req(url_web, headers_pet):
+    return requests.get(url, headers=headers)
+
+
+#region ---Main---
+# Llamada
+response = http_req(url, headers)
+# Comprobar el código de estado de la respuesta
+if response.status_code == 200:
+    try:
+        # Cargamos los datos en formato json
+        data = response.json()
+        # Guardamos el archivo json
+        archivo = f'datos{date.today().strftime("%d%m%Y")}.json'
+        directorio = 'D:\OneDrive\Documentos\datasets\precioluz_json'
+        ruta_completa = os.path.join(directorio, archivo)
+        if not os.path.exists(ruta_completa):
+            os.makedirs(directorio)
+        with open(ruta_completa, "w") as archivo_json:
+            json.dump(data, archivo_json)
+
+
+
+    except json.JSONDecodeError:
+        print("Archivo JSON de la petición dañado")
+        breakpoint()
+else:
+    print(f"Error en la solicitud: {response.status_code}, {response.reason}")
+#endregion
